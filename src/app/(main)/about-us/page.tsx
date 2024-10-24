105690c9e62a208643042703cfc4655ed1a5f1ba
Prakash Acharya <acharyaprakash030@gmail.com>
2024-10-24 11:51:59 +0545

    about us page

---
+++ src/app/(main)/about-us/page.tsx
@@ -0,0 +1,13 @@
+import Hero from '@/components/Homepage/Hero'
+import React from 'react'
+import HeroTemplate from '@/components/Common/HeroTemplate'
+
+const page = () => {
+  return (
+   <>
+  <HeroTemplate pageName='ABOUT US' title="Our Mission: Your Safety" description = "Committed to delivering unmatched security solutions with integrity, professionalism, and excellence."  />
+   </>
+  )
+}
+
+export default page
---
+++ src/components/Common/HeroTemplate.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroTemplate = ({pageName="",title="",description=""}: {pageName: string,title: string,description: string}) => {
+  return (
+    <div className='h-[40vh] bg-gradient-to-r from-[#0F0F0F] via-[#181818] to-[#222222] flex flex-col justify-center items-center gap-5'>
+      <h2 className=" md:text-left text-[#A58441] font-bold text-base">{pageName}</h2>
+      <div className='flex flex-col gap-4 text-center'>
+        <h1 className='lg:text-7xl text-6xl font-medium'>{title}</h1>
+        <h6 className='text-base opacity-80 font-normal'>{description}</h6>
+      </div>
+    </div>
+  )
+}
+
+export default HeroTemplate
