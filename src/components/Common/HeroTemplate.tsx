f8c64c878e969bec3b43959ec9cb3454322d0144
bpn07 <beepinpathak0721@gmail.com>
2024-10-28 13:46:17 +0545

    update

--- src/components/Common/HeroTemplate.tsx
+++ src/components/Common/HeroTemplate.tsx
@@ -8,7 +8,7 @@ const openSans = Open_Sans({
 
 const HeroTemplate = ({pageName,title,description}: {pageName: string,title: string,description: string}) => {
   return (
-    <div className='py-10 bg-gradient-to-r from-[#0F0F0F] via-[#0F0F0F] to-[#222222] flex flex-col justify-center items-center gap-5'>
+    <div className='lg:pt-[120px] lg:pb-10 py-10 bg-gradient-to-r from-[#0F0F0F] via-[#0F0F0F] to-[#222222] flex flex-col justify-center items-center gap-5'>
       <h2 className="md:text-left text-[#A58441] font-bold text-base">{pageName}</h2>
       <div className='flex flex-col gap-4 text-center pb-[72px]'>
         <h1 className='lg:text-7xl text-3xl font-medium'>{title}</h1>
