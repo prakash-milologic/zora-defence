6ec2ec10321fbeb6348ac6586547e378154bb145
bpn07 <beepinpathak0721@gmail.com>
2024-10-28 11:27:45 +0545

    update

--- src/components/Common/HeroTemplate.tsx
+++ src/components/Common/HeroTemplate.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeroTemplate = ({pageName="",title="",description=""}: {pageName: string,title: string,description: string}) => {
   return (
     <div className='h-[40vh] bg-gradient-to-r from-[#0F0F0F] via-[#0F0F0F] to-[#222222] flex flex-col justify-center items-center gap-5'>
-      <h2 className=" md:text-left text-[#A58441] font-bold text-base">{pageName}</h2>
+      <h2 className="md:text-left text-[#A58441] font-bold text-base">{pageName}</h2>
       <div className='flex flex-col gap-4 text-center'>
         <h1 className='lg:text-7xl text-6xl font-medium'>{title}</h1>
         <h6 className='text-base opacity-80 font-normal'>{description}</h6>
