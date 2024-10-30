7a6bf8ea9eacee49446db6a0d99cc9a3139dde6f
Prakash Acharya <acharyaprakash030@gmail.com>
2024-10-30 15:28:19 +0545

    fixing lint

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Hero from "./Hero";
 import About from "./About";
 import Testimonial from "./Testimonial";
-import ContactUs from "./ContactUs";
 import Services from "./Services";
 import FeatureCommonCount from "../Common/FeatureCommonCount";
 import { countItem } from "@/constants/option";
